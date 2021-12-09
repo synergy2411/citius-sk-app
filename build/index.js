d663f9e45d5a41d8a1f2ff49d1de3e91348a3265
Sumit K <synergy2411@outlook.com>
2021-12-09 16:56:24 +0530

    build

--- build/index.js
+++ build/index.js
@@ -4,11 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
 require("./db");
 var morgan_1 = __importDefault(require("morgan"));
 var user_routes_1 = require("./router/user.routes");
 var app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+var PORT = process.env.PORT || 9092;
 app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
 app.use("/users", user_routes_1.UserRouter);
-app.listen(9092, function () { return console.log("Server started at PORT :9092"); });
+app.listen(PORT, function () { return console.log("Server started at PORT :" + PORT); });
